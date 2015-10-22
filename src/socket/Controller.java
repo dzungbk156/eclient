41dbd4f638e0e4720f6676adc9e1970e36903fa9
Zhouyang Xue <ZhouyangXue@Zhouyangs-MacBook-Pro.fritz.box>
2015-10-22 14:21:30 +0200

    controller logs with named

--- src/socket/Controller.java
+++ src/socket/Controller.java
@@ -24,18 +24,18 @@ public class Controller {
 			try {
 
 				socket.connect(hostName,portNumber);
-				log.info("getConnected to "+ hostName + "with " + portNumber);
+				log.info("Controller: getConnected to "+ hostName + "with " + portNumber);
 				
 				s =  socket.receive();
-				log.debug(s);
+				log.debug("Controller: " + s);
 				
 			}
 			catch (ServerException e) {
-				log.error(e.getMessage());
+				log.error("Controller: " + e.getMessage());
 				System.err.println("EchoClient> "+e.getMessage());
 			}
 			catch (IOException e) {
-				log.error(e.getMessage());
+				log.error("Controller: " + e.getMessage());
 				System.err.println("EchoClient> "+e.getMessage());
 			}
 			return s;
@@ -48,16 +48,16 @@ public class Controller {
 			String s = "";
 			try {
 				socket.send(message);
-				log.info("sended message: "+ message);
+				log.info("Controller: sended message: "+ message);
 				s =  socket.receive();
-				log.debug(s);
+				log.debug("Controller: " + s);
 			}
 			catch (ServerException e) {
-				log.error(e.getMessage());
+				log.error("Controller: " + e.getMessage());
 				System.err.println("EchoClient> "+e.getMessage());
 			}
 			catch (IOException e) {
-				log.error(e.getMessage());
+				log.error("Controller: " + e.getMessage());
 				System.err.println("EchoClient> "+e.getMessage());
 			}
 			return s;
@@ -67,20 +67,20 @@ public class Controller {
 			if(socket.isConnect()) {
 				try {
 					socket.disconnect();
-					log.info("socket disconnect");
+					log.info("Controller: socket disconnect");
 				}
 				catch (ServerException e) {
-				log.error(e.getMessage());
-				System.err.println("EchoClient> "+e.getMessage());
+					log.error("Controller: " + e.getMessage());
+					System.err.println("EchoClient> "+e.getMessage());
 				}
 				catch (IOException e) {
-					log.error(e.getMessage());
+					log.error("Controller: " + e.getMessage());
 					System.err.println("EchoClient> "+e.getMessage());
 				}
 				return "Connection terminated: "+ socket.getHostName() + " / "+ socket.getPortNumber();
 			}
 			else {
-				log.error("Try to disconnect but Server Not Connected");
+				log.error("Controller: Try to disconnect but Server Not Connected");
 				System.err.println("EchoClient> Server Not Connected");
 				return "";
 			}
@@ -92,13 +92,13 @@ public class Controller {
 		}
 
 		public String quit() throws IOException  {
-			log.info("quit the application");
+			log.info("Controller: quit the application");
 			if(socket.isConnect()) {
-				log.debug("quit doing disconnect");
+				log.debug("Controller: quit doing disconnect");
 				return disconnect();
 			}
 			else {
-				log.debug("quit without doing disconnect");
+				log.debug("Controller: quit without doing disconnect");
 				return "";
 			}
 
